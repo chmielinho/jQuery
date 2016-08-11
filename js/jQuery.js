f108c6bd6ec5a97b0952fb1e30424b6d6287c6aa
Mateusz Chmielarz <chmielinho@gmail.com>
2016-08-11 02:14:27 +0200

    Done first exercise in jQuery module

---
+++ js/jQuery.js
@@ -0,0 +1,30 @@
+var text;
+function menu() {
+	$('ul').remove();
+	var count = prompt('Ile pozycji listy stworzyć ?');
+	if (count > 0) {
+		$('#menu').append('<ul></ul>');
+		for (var i = 0; i < count; i++) {
+			text = prompt('Podaj text dla pozycji nr ' + (i + 1) + ' :');
+			if (text === null || text === '') {
+				text = 'Nowa pozycja listy';
+			}
+			$('ul').append('<li>' + text + ' </li>');
+		}
+	}
+};
+$('.button').click(menu);
+
+$('span').each(function (index, element) {
+	if (index % 2 === 0) {
+		$(element).css('color', 'red');
+	}
+});
+var paragraphs = $('.buttons').find('p');
+paragraphs.each(function (index, element) {
+	var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
+	$(element).append(button);
+});
+$('.btn').click(function () {
+	alert($(this).attr('data-tmp'));
+});
